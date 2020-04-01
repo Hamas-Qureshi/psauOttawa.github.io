db17f6a71ffd06b864cedcda239a2034bb88798c
Hamas-Qureshi <55336659+Hamas-Qureshi@users.noreply.github.com>
2020-04-01 12:15:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
 <?php 
-
-    if (isset($_POST['submit'])) {
         $name = $_POST['name'];
-        $mailForm = $_POST['mail'];
+        $visitor_email = $_POST['email'];
         $message = $_POST['message'];
         
         
-        $mailTo = "sdcdsc";
-        $headers = "From: ".$mailFrom;
-        $txt= "You have received an e-mail from ".$name.".\n\n".$message;
-            
-            
-        mail($mailTo,$subject,$txt,$headers);
-        header("Location: index.php?mailsend");
+        $email_from = "hammas0400@gmail.com";
+        $email_body = "User Name: $name.\n".
+                        "User Email : $visitor_email.\n".
+                        "User message : $message.\n";
+
+
+
+        $to = "psa.ottawau@gmail.com"
+
+        $headers= "From: $email_from \r\n";
+
+        $headers .="Reply-To: $visitor_email \r\n";
+    
+        mail($to,$email_body,$headers);
+        header("Location: index.html");
                
-    }
 ?>
+
+
 
 
 
